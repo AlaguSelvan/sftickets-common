9a5f283b46dfe61171dc636c54f8ebf8a9f6a278
alaguselvan <alaguselvan97@gmail.com>
2021-01-31 16:18:17 +0530

    Updates

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -1,12 +1,12 @@
 import { Subjects } from './subjects'
 
-export interface TicketUpdatedEvent {
-  subject: Subjects.TicketUpdated;
-  data: {
+	export interface TicketUpdatedEvent {
+	subject: Subjects.TicketUpdated;
+	data: {
 		id: string;
 		version: number;
-    title: string;
-    price: number;
-    userId: string;
-  }
-}
+		title: string;
+		price: number;
+		userId: string;
+	}
+	}
